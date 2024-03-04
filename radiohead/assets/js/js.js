0e870c0b34bc442a278f8c216065d545db838e77
Jan Schwegler <schwegler.jan@gmail.com>
2024-03-04 17:43:49 +0100

    add css animations to the bars

--- radiohead/assets/js/js.js
+++ radiohead/assets/js/js.js
@@ -5,7 +5,7 @@ function createBars() {
     let barContainer = document.getElementById("barsAnimate");
     let barContainerHeight = window.innerHeight - document.getElementsByTagName("main")[0].clientHeight;
     let barCount = Math.round(Math.abs(barContainerHeight) / (barHeight + barSpaceing));
-    let barAdaptHeight = (barContainerHeight - (barSpaceing * (barCount + 1))) / barCount;
+    let barAdaptHeight = (barContainerHeight - (barSpaceing * (barCount))) / barCount;
 
     barContainer.style.paddingTop = barSpaceing + "px";
 
@@ -26,6 +26,8 @@ function createBars() {
     Array.from(barContainer.getElementsByTagName('div')).forEach(div => {
         div.style.height = barAdaptHeight + 'px';
         div.style.marginBottom = barSpaceing + 'px';
+        div.style.animationName = "bar" + (Math.floor(Math.random() * 6) + 1);
+        div.style.animationDuration = (Math.floor(Math.random() * 4) + 3) + "s";
     });
 }
 
