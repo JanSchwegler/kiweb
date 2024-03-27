d9712f4ccd90304ac1e8ce3a8cb00aa2302c9f14
Jan Schwegler <schwegler.jan@gmail.com>
2024-03-27 21:10:34 +0100

    create bar script

--- v3_radiohead/assets/bars.js
+++ v3_radiohead/assets/bars.js
@@ -1,19 +1,37 @@
-// var
-let bars;
-
-// definitions, calls & listeners
 window.onload = function() {
-    bars = document.querySelectorAll(".bars div");
+    createBars();
+}
 
+window.addEventListener('resize', () => {
+    createBars();
+});
+
+function createBars() {
+    let barParent = document.querySelectorAll(".bars");
+    // del existing bars
+    barParent.forEach((e) => {
+        while (e.firstChild) {
+            e.removeChild(e.firstChild);
+        }
+    });
+    // create bars
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let barCount = Math.floor((height - ((0.23 * width + 50))) / 55) + 1;
+    barParent.forEach((e) => {
+        for (let i = 0; i < barCount; i++) {
+            e.appendChild(document.createElement("div"));
+        }
+    });
     animateBars();
 }
 
-// functions
 function animateBars() {
+    let bars = document.querySelectorAll(".bars div");
     let styleE = document.createElement("style");
     document.body.insertBefore(styleE, document.body.firstChild);
     bars.forEach((e, i) => {
-        let randomStart = Math.floor(Math.random() * 41) + 60;
+        let randomStart = Math.floor(Math.random() * 41) + 60; // do while function
         styleE.innerHTML += `@keyframes bar${i} {
             0%   {
                 width: ${randomStart}%;
